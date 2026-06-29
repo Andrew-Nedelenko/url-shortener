f51207c58dc49166aeeba7000ec1796c4e3ea439
sntone <areyns7857@gmail.com>
2026-06-29 12:04:08 +0300

    feat: add postgres, typeorm, docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UrlShortenModule } from './url-shorten/url-shorten.module';
+import { databaseConfig } from './config/database.config';
 
 @Module({
-  imports: [UrlShortenModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(databaseConfig),
+    UrlShortenModule,
+  ],
 })
 export class AppModule {}
---
+++ src/config/database.config.ts
@@ -0,0 +1,16 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+export const databaseConfig: TypeOrmModuleAsyncOptions = {
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => ({
+    type: 'postgres' as const,
+    host: config.get<string>('DB_HOST', 'localhost'),
+    port: config.get<number>('DB_PORT', 5432),
+    username: config.get<string>('DB_USERNAME', 'postgres'),
+    password: config.get<string>('DB_PASSWORD', 'postgres'),
+    database: config.get<string>('DB_DATABASE', 'url_shortener'),
+    autoLoadEntities: true,
+    synchronize: false,
+  }),
+};
